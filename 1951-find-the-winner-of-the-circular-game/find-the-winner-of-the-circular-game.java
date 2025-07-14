573c7a58cc1cd6ef2a38f90152103ffae2d5ef72
Thrisha <thrish.ms@gmail.com>
2025-07-14 10:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (88.72%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        int ind = helper(n,k);
+        return ind+1;
+    }
+    int helper(int n, int k){
+        if(n==1)
+            return 0;
+        return (helper(n-1,k)+k)%n;
+    }
+}
