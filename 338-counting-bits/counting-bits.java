51184474d88bf8d0706c5d78c38335e359096d6e
Thrisha <thrish.ms@gmail.com>
2025-07-18 10:27:53 +0530

    Time: 2 ms (94.17%) | Memory: 46.6 MB (83.85%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,5 +1,23 @@
 class Solution {
-    public int[] countBits(int n) {
+    public int[] countBits(int n){
+        if(n==0)
+            return new int[]{0};
+        if(n==1)
+            return new int[]{0,1};
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i/2]+i%2;
+        }
+        return dp;
+    }
+
+
+
+
+    /*public int[] countBits(int n) {
         int[] count = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             count[i] = countSetBits(i);
@@ -14,5 +32,5 @@ class Solution {
             n /= 2;
         }
         return cnt;
-    }
+    }*/
 }
