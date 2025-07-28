88a375d94eaf2c7d1b7207850819211d6b11998d
Thrisha <thrish.ms@gmail.com>
2025-07-28 14:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (60.48%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,21 +3,16 @@ class Solution {
         int k=0,i=0,j=0;
         int[] res = new int[m+n];
         while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-               res[k++]=nums1[i++];
-            }
-            else{
-                res[k++]=nums2[j++];
-            }
+            if(nums1[i]<nums2[j])
+               res[k++]=nums1[i++];          
+            else
+                res[k++]=nums2[j++];    
         }
-        while(i<m){
+        while(i<m)
            res[k++]=nums1[i++]; 
-        }
-        while(j<n){
+        while(j<n)
             res[k++]=nums2[j++];
-        }
-        for(i=0;i<(m+n);i++){
+        for(i=0;i<(m+n);i++)
             nums1[i]=res[i];
-        }
     }
 }
